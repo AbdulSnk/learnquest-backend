98da372edec996ef2e59ba5b46dd56aaf39529e7
AbdulSnk <sirmee7@gmail.com>
2026-01-21 00:32:26 +0100

    chore(config): setup core configuration, environment validation, and DB connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable global validation for all incoming requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,        // remove extra fields not in DTO
+      forbidNonWhitelisted: true, // throw error if extra fields exist
+      transform: true,        // automatically transform types
+    }),
+  );
+
+  // Enable graceful shutdown hooks
+  app.enableShutdownHooks();
+
+ 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 App running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`);
+
 }
 bootstrap();
